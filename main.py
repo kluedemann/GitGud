ccd3d391222129b4080396d2f4649312116b5bf9
Steven Tang <18170455+steventango@users.noreply.github.com>
2022-01-15 16:32:01 -0700

    Draw some dots

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 from pygame.locals import *
+import math
+
 
 class Game():
 
-    def __init__(self, surface: pygame.Surface, l: int = 3, w: int = 3, h: int = 3):
+    def __init__(
+            self,
+            surface: pygame.Surface,
+            l: int = 3,
+            w: int = 3,
+            h: int = 3):
         self.surface = surface
         self.bg_color = pygame.Color('black')
-        width = self.surface.get_width()//3
-        height = self.surface.get_height()//3
+        width = self.surface.get_width() // 3
+        height = self.surface.get_height() // 3
 
         self.board = [
             [
@@ -20,17 +27,46 @@ class Game():
         self.w = w
         self.h = h
 
-
     def draw(self):
+        angle = math.pi / 3
+        offset = {
+            'x': self.surface.get_width() / 2,
+            'y': self.surface.get_height() / 2
+        }
+
         for z, board in enumerate(self.board):
             for x, row in enumerate(board):
                 for y, cube in enumerate(row):
                     if cube == 'X':
-                        pygame.draw.circle(self.surface, (255, 0, 0), (x * 100, y * 100), 5)
+                        pygame.draw.circle(
+                            self.surface,
+                            (255, 0, 0),
+                            (
+                                offset['x'] + 100 * x * math.cos(angle),
+                                offset['y'] + 100 * y * math.sin(angle)
+                            ),
+                            5
+                        )
                     elif cube == 'O':
-                        pygame.draw.circle(self.surface, (0, 0, 255), (x * 100, y * 100), 5)
+                        pygame.draw.circle(
+                            self.surface,
+                            (0, 0, 255),
+                            (
+                                offset['x'] + 100 * x * math.cos(angle),
+                                offset['y'] + 100 * y * math.sin(angle)
+                            ),
+                            5
+                        )
                     else:
-                        pygame.draw.circle(self.surface, (255, 255, 255), (x * 100, y * 100), 5)
+                        pygame.draw.circle(
+                            self.surface,
+                            (255, 255, 255),
+                            (
+                                offset['x'] + 100 * x * math.cos(angle),
+                                offset['y'] + 100 * y * math.sin(angle)
+                            ),
+                            5
+                        )
 
 
     def turn(self, player: str, x: int, y: int, z: int) -> bool:
@@ -41,18 +77,16 @@ class Game():
 
 
     def play(self):
-        while True:
-            self.draw()
-            screen.blit(self.surfacebackground, (0, 0))
-            pygame.display.flip()
-            input()
+        self.draw()
+        pygame.display.update()
+        input()
 
 
     def __str__(self) -> str:
         string = ''
         for z in self.board:
             for x in z:
-                string += ' '.join(map(lambda y: f'[{y}]',x))
+                string += ' '.join(map(lambda y: f'[{y}]', x))
                 string += '\n'
             string += '\n'
         return string
@@ -73,6 +107,7 @@ def main():
     game.play()
     # quit pygame and clean up the pygame window
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
