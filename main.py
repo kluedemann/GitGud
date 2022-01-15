e21cff370cd22e05f7986acbb2a1ea2580f477a6
Steven Tang <18170455+steventango@users.noreply.github.com>
2022-01-15 16:19:27 -0700

    Draw board code

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import *
 
 class Game():
-    def __init__(self, l: int, w: int, h: int):
+    def __init__(self, surface: pygame.Surface, l: int = 3, w: int = 3, h: int = 3):
+        self.surface = surface
         self.board = [
             [
                 [
@@ -14,11 +15,33 @@ class Game():
         self.w = w
         self.h = h
 
+
+    def draw(self):
+        for z, board in enumerate(self.board):
+            for x, row in enumerate(board):
+                for y, cube in enumerate(row):
+                    if cube == 'X':
+                        pygame.draw.circle(self.surface, (255, 0, 0), (x * 100, y * 100), 5)
+                    elif cube == 'O':
+                        pygame.draw.circle(self.surface, (0, 0, 255), (x * 100, y * 100), 5)
+                    else:
+                        pygame.draw.circle(self.surface, (255, 255, 255), (x * 100, y * 100), 5)
+
+
     def turn(self, player: str, x: int, y: int, z: int) -> bool:
         if self.board[z][x][y] == ' ':
             self.board[z][x][y] = player
             return True
         return False
+
+
+    def play(self):
+        while True:
+            self.draw()
+            screen.blit(self.surfacebackground, (0, 0))
+            pygame.display.flip()
+            input()
+
 
     def __str__(self) -> str:
         string = ''
@@ -31,12 +54,6 @@ class Game():
 
 
 def main():
-    game = Game(3, 3, 3)
-
-    print(game)
-    game.turn('X', 0, 0, 0)
-    print(game)
-
     # initialize pygame modules
     pygame.init()
     # create display window
