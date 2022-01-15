b996664a3bf66dd0960d35bf29d9347b2a3a5976
J-101 <91110170+J-101@users.noreply.github.com>
2022-01-15 15:36:36 -0700

    Main stuff

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pygame
 
+def main():
+    # initialize pygame modules
+    pygame.init()
+    # create display window
+    pygame.display.set_mode((600, 600))
+    # title display window
+    pygame.display.set_caption('Tic Tac Toe')   
+    # get display surface
+    w_surface = pygame.display.get_surface() 
+    # create a game object
+    game = Game(w_surface)
+    # begin the main game loop calling the play method on the game object
+    game.play() 
+    # quit pygame and clean up the pygame window
+    pygame.quit()
